91b54097c55722616b59d10884e05e2be5da48ef
Tomás Alberdi <tomas.alberdi.2001@gmail.com>
2026-02-02 13:38:52 -0300

    removed partial client DTO

--- backend/src/main/java/d10/backend/Mapper/ClientMapper.java
+++ backend/src/main/java/d10/backend/Mapper/ClientMapper.java
@@ -2,7 +2,6 @@ package d10.backend.Mapper;
 
 
 import d10.backend.DTO.Client.CreateClientDTO;
-import d10.backend.DTO.Client.PartialClientDTO;
 import d10.backend.Model.Client;
 
 public class ClientMapper {
@@ -20,14 +19,6 @@ public class ClientMapper {
         client.setCuitDni(dto.getCuitDni());
         client.setEmail(dto.getEmail());
         client.setPhone(dto.getPhone());
-    }
-
-    public static PartialClientDTO toPartialDTO(Client client) {
-        PartialClientDTO partialClientDTO = new PartialClientDTO();
-        partialClientDTO.setId(partialClientDTO.getId());
-        partialClientDTO.setName(client.getName());
-        partialClientDTO.setType(client.getType());
-        return partialClientDTO;
     }
 
 }
